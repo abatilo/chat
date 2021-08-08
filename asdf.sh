588400b87bef6652bc85fb2bbda7d12e206a1588
Aaron <AaronBatilo@gmail.com>
2021-08-08 08:22:16 -0600

    Include pulumi with installed tools

--- asdf.sh
+++ asdf.sh
@@ -7,3 +7,7 @@ asdf plugin-add chamber https://github.com/mintel/asdf-chamber.git
 asdf install chamber 2.10.1
 asdf plugin-add tilt https://github.com/eaceaser/asdf-tilt.git
 asdf install tilt 0.22.3
+asdf plugin-add gomigrate https://github.com/joschi/asdf-gomigrate
+asdf install gomigrate 4.14.1
+asdf plugin-add pulumi https://github.com/canha/asdf-pulumi.git
+asdf install pulumi 3.9.1
